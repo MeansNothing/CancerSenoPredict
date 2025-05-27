c299d8ace1430eac42843c4b88e3ab2a47484976
MeansNothing <joseu10playa@gmail.com>
2025-05-27 18:21:49 -0500

    first commit

---
+++ production.py
@@ -0,0 +1,21 @@
+from .settings import *
+import os
+
+DEBUG = False
+ALLOWED_HOSTS = ['*']
+
+# Configuración de la base de datos para producción (usando PostgreSQL en Render)
+DATABASES = {
+    'default': {
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': os.getenv('PGDATABASE'),
+        'USER': os.getenv('PGUSER'),
+        'PASSWORD': os.getenv('PGPASSWORD'),
+        'HOST': os.getenv('PGHOST'),
+        'PORT': os.getenv('PGPORT', '5432'),
+    }
+}
+
+# Configuración de archivos estáticos
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
+STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
